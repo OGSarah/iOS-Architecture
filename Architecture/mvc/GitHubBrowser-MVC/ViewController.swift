026fd8ea9b61ee338868f7eaea54c9d7e9d0f94f
Sarah Clark <sarahireneclark@gmail.com>
2026-07-15 09:18:06 -0500

    Initial commit of MVC project

---
+++ Architecture/mvc/GitHubBrowser-MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubBrowser-MVC
+//
+//  Created by Sarah Clark on 7/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Architecture/mvc/GitHubBrowser-MVCTests/GitHubBrowser_MVCTests.swift
@@ -0,0 +1,19 @@
+//
+//  GitHubBrowser_MVCTests.swift
+//  GitHubBrowser-MVCTests
+//
+//  Created by Sarah Clark on 7/15/26.
+//
+
+import Testing
+@testable import GitHubBrowser_MVC
+
+struct GitHubBrowser_MVCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
