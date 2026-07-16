8af5a33654b4c493b11eabcaf160fcbc0288ce10
Sarah Clark <sarahireneclark@gmail.com>
2026-07-16 06:17:09 -0500

    Initial commit of AuroraWatch-MVVM project

---
+++ Architecture/mvvm/AuroraWatch-MVVM Watch App/AuroraWatch_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraWatch_MVVMApp.swift
+//  AuroraWatch-MVVM Watch App
+//
+//  Created by Sarah Clark on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct AuroraWatch_MVVM_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Architecture/mvvm/AuroraWatch-MVVM Watch AppTests/AuroraWatch_MVVM_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  AuroraWatch_MVVM_Watch_AppTests.swift
+//  AuroraWatch-MVVM Watch AppTests
+//
+//  Created by Sarah Clark on 7/16/26.
+//
+
+import Testing
+@testable import AuroraWatch_MVVM_Watch_App
+
+struct AuroraWatch_MVVM_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
